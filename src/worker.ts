dc65f95f1059cd71be172cc1631e82df814a3208
Lawrence Aritao <76676277+lawrenceaph@users.noreply.github.com>
2023-08-10 19:33:41 +0800

    added some meta tags

--- src/worker.ts
+++ src/worker.ts
@@ -24,14 +24,18 @@ export interface Env {
 	// MY_QUEUE: Queue;
 }
 
-
 function generateHelpResponse() {
-    return `
+	return `
         <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta property="og:url" content="https://nftimage.pilipinas.dev/">
+            <meta property="og:type" content="website">
+            <meta property="og:title" content="Placeholder NFT Images">
+            <meta property="og:description" content="A tiny api for sequential image generation">
+            <meta property="og:image" content="https://imagedelivery.net/HML6qmlXDXx6EPV6zNm9VA/c7e675af-9ace-42ff-64a6-f2dedf2da400/public">
             <title>API Usage Guide</title>
             <style>
                 body {
@@ -63,69 +67,68 @@ function generateHelpResponse() {
             <div class="container">
                 <h1>API Usage Guide</h1>
                 <p>To generate an SVG with a number:</p>
+                <code >https://nftimage.pilipinas.dev/[NUMBER].svg</code>
+                <br>
+                or 
+                <br><br>
                 <code >https://nftimage.pilipinas.dev/[NUMBER].png</code>
-                <p>Replace [NUMBER] with the desired number. This api can provide low-bandwidth SVGs sequentially. Enjoy!</p>
+                <p>Replace [NUMBER] with the desired number. This api can provide lightweight SVGs sequentially. Enjoy!</p>
             </div>
         </body>
         </html>
     `;
 }
-function mixColor(color1:any, color2:any, weight:any) {
-    let w1 = weight;
-    let w2 = 1 - w1;
-    let rgb = [Math.round(color1[0] * w1 + color2[0] * w2),
-               Math.round(color1[1] * w1 + color2[1] * w2),
-               Math.round(color1[2] * w1 + color2[1] * w2)];
-    return rgb;
+function mixColor(color1: any, color2: any, weight: any) {
+	let w1 = weight;
+	let w2 = 1 - w1;
+	let rgb = [
+		Math.round(color1[0] * w1 + color2[0] * w2),
+		Math.round(color1[1] * w1 + color2[1] * w2),
+		Math.round(color1[2] * w1 + color2[1] * w2),
+	];
+	return rgb;
 }
 
 function randomPastelColor(dark = false) {
-    const randomRGB = [Math.random() * 256, Math.random() * 256, Math.random() * 256];
-    const white = [255, 255, 255];
-    const darkColor = [85, 85, 85]; // This can be adjusted for desired darkness
+	const randomRGB = [Math.random() * 256, Math.random() * 256, Math.random() * 256];
+	const white = [255, 255, 255];
+	const darkColor = [85, 85, 85]; // This can be adjusted for desired darkness
 
-    if (dark) {
-        return `rgb(${mixColor(randomRGB, darkColor, 0.5).join(",")})`;
-    } else {
-        return `rgb(${mixColor(randomRGB, white, 0.5).join(",")})`;
-    }
+	if (dark) {
+		return `rgb(${mixColor(randomRGB, darkColor, 0.5).join(',')})`;
+	} else {
+		return `rgb(${mixColor(randomRGB, white, 0.5).join(',')})`;
+	}
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const url = new URL(request.url);
+		const numberMatch = url.pathname.match(/\/(\d+)\.png/) || url.pathname.match(/\/(\d+)\.svg/);
+		if (!numberMatch) {
+			return new Response(generateHelpResponse(), {
+				status: 200,
+				headers: {
+					'Content-Type': 'text/html',
+				},
+			});
+		}
 
-
-
-
-const url = new URL(request.url);
-  const numberMatch = url.pathname.match(/\/(\d+)\.png/);
-		 if (!numberMatch) {
-    return new Response(
-				generateHelpResponse(), {
-					status:200,
-					headers: {
-						'Content-Type': 'text/html'
-					}
-				}
-			);
-  }
-
-		 const number = numberMatch[1];
-  const textColor = randomPastelColor(true);
+		const number = numberMatch[1];
+		const textColor = randomPastelColor(true);
 		const backgroundColor = randomPastelColor();
-  const svg = `
+		const svg = `
     <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
 <rect width="200" height="200" fill="${backgroundColor}" rx="20" ry="20"></rect>
       <text x="100" y="100" font-family="Arial" font-size="60" text-anchor="middle" fill="${textColor}">${number}</text>
     </svg>
   `;
 
-
 		return new Response(svg, {
 			status: 200,
 			headers: {
-				'content-type': 'image/svg+xml'
-			}
+				'content-type': 'image/svg+xml',
+			},
 		});
 	},
 };
